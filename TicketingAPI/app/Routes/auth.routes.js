9f0c672dabd67c51c0e6d337e4091ede3367c2d8
mayurpatil0777 <mayur.patil0777@gmail.com>
2019-10-19 16:52:43 +0530

    api issue resolved

--- TicketingAPI/app/Routes/auth.routes.js
+++ TicketingAPI/app/Routes/auth.routes.js
@@ -5,14 +5,14 @@ module.exports = (app) => {
     app.post('/notes', notes.create);
 
     // Retrieve all Notes
-    app.get('/notes', notes.findAll);
+    // app.get('/notes', notes.findAll);
 
-    // Retrieve a single Note with noteId
-    app.get('/notes/:noteId', notes.findOne);
+    // // Retrieve a single Note with noteId
+    // app.get('/notes/:noteId', notes.findOne);
 
-    // Update a Note with noteId
-    app.put('/notes/:noteId', notes.update);
+    // // Update a Note with noteId
+    // app.put('/notes/:noteId', notes.update);
 
-    // Delete a Note with noteId
-    app.delete('/notes/:noteId', notes.delete);
+    // // Delete a Note with noteId
+    // app.delete('/notes/:noteId', notes.delete);
 }
